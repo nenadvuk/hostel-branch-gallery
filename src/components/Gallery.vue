a907c70c18267874907dda88201a61e27675f690
nenadvuk <nenadvukasinovic.rs@gmail.com>
2019-07-13 01:11:12 +0200

    small changes

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,125 +1,118 @@
 <template>
   <div id="app">
+    <div class="gallery-box">
     <v-app id="inspire">
-      <v-layout justify-center>
-        <v-flex xs12 sm6>
-          <v-navigation-drawer temporary v-model="sideNav" absolute dark class="providna">
-            <v-list>
-              <v-list-tile v-for="item in menuItems" :key="item.title">
-                <v-list-tile-action>
-                  <v-icon>{{ item.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-              </v-list-tile>
-            </v-list>
-          </v-navigation-drawer>
-          <v-toolbar fixed dark class="dark3g">
-            <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
-            <v-toolbar-title>
-              Gallery
-              <span class="font-weight-light">Hostel Branch</span>
-            </v-toolbar-title>
+        <v-layout justify-center>
+          <v-flex xs12 sm6>
+            <v-navigation-drawer temporary v-model="sideNav" absolute dark class="providna">
+              <v-list>
+                <v-list-tile v-for="item in menuItems" :key="item.title">
+                  <v-list-tile-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-navigation-drawer>
+            <v-toolbar fixed dark class="dark3g">
+              <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
+              <v-toolbar-title>
+                Gallery
+                <span class="font-weight-light">Hostel Branch</span>
+              </v-toolbar-title>
 
-            <v-spacer></v-spacer>
-            <v-toolbar-items class="hidden-sm-and-down">
-              <v-btn flat v-for="item in menuItems" :key="item.title">{{ item.title }}</v-btn>
-            </v-toolbar-items>
-          </v-toolbar>
-        </v-flex>
-      </v-layout>
-      
-      <v-layout column>
-        <v-flex xs12>
-          <v-layout align-space-between justify-space-around fill-height fill-width>
-            <v-hover>
-              <v-card
-                dark
-                color="primary"
-                slot-scope="{ hover }"
-                :class="`elevation-${hover ? 24 : 4}`"
-                class="mx-2"
-                width="400"
-              >
-                <v-img
-                  :aspect-ratio="16/9"
-                  src="/img/branch/smestaj/smestaj19.jpg"
-                ></v-img>
-                <v-card-title >
-                  <div>
-                  <span class="title ">Smeštaj</span>
-                    <div class="d-flex">
-                      <div class="ml-2 grey--text text--darken-2">
-                        <v-btn round >
-                          <router-link to="/Accommodation">Open</router-link>
-                        </v-btn>
-                      </div>
-                    </div>
-                  </div>
-                  <v-spacer></v-spacer>
-                </v-card-title>
-              </v-card>
-            </v-hover>
-            
-            <v-hover>
-              <v-card
-                dark
-                slot-scope="{ hover }"
-                :class="`elevation-${hover ? 24 : 4}`"
-                class="mx-2"
-                width="400"
-              > 
-                <v-img
-                  :aspect-ratio="16/9"
-                  src="/img/branch/dvoriste/dvoriste1.jpg"
-                ></v-img>
-                
-                <v-card-title>
-                  <div>
-                    <span class="title">Dvorište</span>
-                    <div class="d-flex">
-                      <div class="ml-2 grey--text text--darken-2">
-                        <v-btn round >
-                          <router-link to="/Yard">Open</router-link>
-                        </v-btn>
-                      </div>
-                    </div>
-                  </div>
-                  <v-spacer></v-spacer>
-                </v-card-title>
-              </v-card>
-            </v-hover>
+              <v-spacer></v-spacer>
+              <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat v-for="item in menuItems" :key="item.title">{{ item.title }}</v-btn>
+              </v-toolbar-items>
+            </v-toolbar>
+          </v-flex>
+        </v-layout>
 
-            <v-hover>
-              <v-card
-                dark
-                slot-scope="{ hover }"
-                :class="`elevation-${hover ? 24 : 4}`"
-                class="mx-2"
-                width="400"
-              >
-                <v-img
-                  :aspect-ratio="16/9"
-                  src="/img/meetup-3g/meetup11.jpg"
-                ></v-img>
-                <v-card-title>
-                  <div>
-                    <span class="title">3G MeetUp</span>
-                    <div class="d-flex">
-                      <div class="ml-2 grey--text text--darken-2">
-                        <v-btn round  >
-                          <router-link to="/MeetUp3G">Open</router-link>
-                        </v-btn>
+        <v-layout column>
+          <v-flex xs12>
+            <v-layout align-space-between justify-space-around fill-height fill-width>
+              <v-hover>
+                <v-card
+                  dark
+                  color="primary"
+                  slot-scope="{ hover }"
+                  :class="`elevation-${hover ? 24 : 4}`"
+                  class="mx-2"
+                  width="400"
+                >
+                  <v-img :aspect-ratio="16/9" src="/img/branch/smestaj/smestaj19.jpg"></v-img>
+                  <v-card-title>
+                    <div>
+                      <span class="title">Smeštaj</span>
+                      <div class="d-flex">
+                        <div class="ml-2 grey--text text--darken-2">
+                          <v-btn round>
+                            <router-link to="/Accommodation">Open</router-link>
+                          </v-btn>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <v-spacer></v-spacer>
-                </v-card-title>
-              </v-card>
-            </v-hover>
-          </v-layout>
-        </v-flex>
-      </v-layout>
+                    <v-spacer></v-spacer>
+                  </v-card-title>
+                </v-card>
+              </v-hover>
+
+              <v-hover>
+                <v-card
+                  dark
+                  slot-scope="{ hover }"
+                  :class="`elevation-${hover ? 24 : 4}`"
+                  class="mx-2"
+                  width="400"
+                >
+                  <v-img :aspect-ratio="16/9" src="/img/branch/dvoriste/dvoriste1.jpg"></v-img>
+
+                  <v-card-title>
+                    <div>
+                      <span class="title">Dvorište</span>
+                      <div class="d-flex">
+                        <div class="ml-2 grey--text text--darken-2">
+                          <v-btn round>
+                            <router-link to="/Yard">Open</router-link>
+                          </v-btn>
+                        </div>
+                      </div>
+                    </div>
+                    <v-spacer></v-spacer>
+                  </v-card-title>
+                </v-card>
+              </v-hover>
+
+              <v-hover>
+                <v-card
+                  dark
+                  slot-scope="{ hover }"
+                  :class="`elevation-${hover ? 24 : 4}`"
+                  class="mx-2"
+                  width="400"
+                >
+                  <v-img :aspect-ratio="16/9" src="/img/meetup-3g/meetup11.jpg"></v-img>
+                  <v-card-title>
+                    <div>
+                      <span class="title">3G MeetUp</span>
+                      <div class="d-flex">
+                        <div class="ml-2 grey--text text--darken-2">
+                          <v-btn round>
+                            <router-link to="/MeetUp3G">Open</router-link>
+                          </v-btn>
+                        </div>
+                      </div>
+                    </div>
+                    <v-spacer></v-spacer>
+                  </v-card-title>
+                </v-card>
+              </v-hover>
+            </v-layout>
+          </v-flex>
+        </v-layout>
     </v-app>
+    </div>
   </div>
 </template>
 
@@ -162,9 +155,10 @@ export default {
 </script>
 
 <style>
-@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css";
+
 html {
   scroll-behavior: smooth;
+
 }
 
 .resultContainer {
@@ -175,5 +169,10 @@ html {
   min-height: 50px;
   min-width: 80px;
   margin: 10px;
+}
+
+.layout {
+  background-color: rgba(199, 253, 253, 0.788);
+
 }
 </style>
--- src/components/Yard.vue
+++ src/components/Yard.vue
@@ -1,41 +1,43 @@
 <template>
-  <v-app>
-    <v-navigation-drawer temporary v-model="sideNav" absolute dark class="providna">
-      <v-list>
-        <v-list-tile v-for="item in menuItems" :key="item.title">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar fixed dark class="dark3g">
-      <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
-      <v-toolbar-title>
-        Eco
-        <span class="font-weight-light">Hostel Branch</span>
-      </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat v-for="item in menuItems" :key="item.title">{{ item.title }}</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <v-container>
-      <section class="img-box">
-        <h2>Courtyard</h2>
-        <Lightbox
-          :thumbnails="lightbox.images.thumbnails"
-          :largeImages="lightbox.images.large"
-          :thumbnailsPath="lightbox.thumbnailsPath"
-          :largePath="lightbox.largePath"
-          :captions="lightbox.captions"
-          class="lightBox"
-        />
-      </section>
-    </v-container>
-  </v-app>
+  <div id="app">
+    <v-app id="inspire">
+      <v-container>
+        <v-navigation-drawer temporary v-model="sideNav" absolute dark class="providna">
+          <v-list>
+            <v-list-tile v-for="item in menuItems" :key="item.title">
+              <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-navigation-drawer>
+        <v-toolbar fixed dark class="dark3g">
+          <v-toolbar-side-icon @click="sideNav = !sideNav" class="hidden-md-and-up"></v-toolbar-side-icon>
+          <v-toolbar-title>
+            Gallery
+            <span class="font-weight-light">Hostel Branch</span>
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat v-for="item in menuItems" :key="item.title">{{ item.title }}</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+        
+        <section class="img-box">
+          <h2>Courtyard</h2>
+          <Lightbox
+            :thumbnails="lightbox.images.thumbnails"
+            :largeImages="lightbox.images.large"
+            :thumbnailsPath="lightbox.thumbnailsPath"
+            :largePath="lightbox.largePath"
+            :captions="lightbox.captions"
+            class="lightBox"
+          />
+        </section>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
